3c9f72aa0bdbfe28fcc99ebfa2fd01d4a5727ce8
hanhmchau <hanhmchau@outlook.com>
2018-09-19 16:33:13 +0700

    Update api

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ console.log(process.env.DATASOURCE);
 const apiRouters = require('./router');
 const cors = require('cors');
 
+const public = path.resolve(__dirname, 'public');
+app.use(express.static(public));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: true
@@ -15,25 +18,10 @@ app.use(bodyParser.json())
 
 app.use('/api', cors(), apiRouters);
 
-const public = path.resolve(__dirname, 'public');
-app.use(express.static(public));
-
 // send the user to index html page
 app.get('/', (req, res) => {
     res.sendFile(public);
 });
-
-// // talk about API naming convention
-// app.route('/book')
-//     .get((req, res) => {
-//         res.send('Get a random book');
-//     })
-//     .post((req, res) => {
-//         res.send('Add a book');
-//     })
-//     .put((req, res) => {
-//         res.send('Update the book');
-//     })
 
 // last route, catches 404
 app.use((req, res) => {
